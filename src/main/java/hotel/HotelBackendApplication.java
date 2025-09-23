62c83d33eeff21df870c23d6f6a21e12356def13
2300031850 <163657833+2300031850@users.noreply.github.com>
2025-09-23 15:06:54 +0530

    HotelBackendApplication.java

--- src/main/java/hotel/HotelBackendApplication.java
+++ src/main/java/hotel/HotelBackendApplication.java
@@ -2,12 +2,18 @@ package hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HotelBackendApplication {
+public class HotelBackendApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HotelBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBackendApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HotelBackendApplication.class);
+    }
 }
